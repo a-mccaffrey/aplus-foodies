66d711699a64016ac89a58564fa61815ae47f04c
Dasa Riadi <DASA.RIADI97@GMAIL.COM>
2020-06-22 21:20:47 -0400

    commit

--- script.js
+++ script.js
@@ -5,12 +5,48 @@
 // This is our API key.
 var APIKey = "75880f1a2313ab8b5d85907f998f30f7";
 
-// Here we are building the URL we need to query the database
-var ingredient = "noodle"
-var queryURL = "https://api.edamam.com/search?q=" + ingredient + "&app_id=ff0891d5" + "&app_key=75880f1a2313ab8b5d85907f998f30f7"; // + "&from=0&to=3&calories=591-722&health=alcohol-free";
-console.log(queryURL);
 
-// // array to add cities to, to be grabbed from after search
-// var citiesArray = JSON.parse(localStorage.getItem("cities")) || [];
+$("#find-food").on("click", function(event) {
 
-// const m = moment();
+    // event.preventDefault() can be used to prevent an event's default behavior.
+    // Here, it prevents the submit button from trying to submit a form when clicked
+    event.preventDefault();
+
+    // Here we grab the text from the input box
+    var strFood = $("#food-input").val();
+
+    // Here we construct our URL
+    var queryURL = "https://api.edamam.com/search?q=" + strFood + "&app_id=ff0891d5" + "&app_key=75880f1a2313ab8b5d85907f998f30f7" + "&from=0&to=3";
+
+    // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
+    // and display it in the div with an id of food-view
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+
+        // creating a food div to hold the food
+        var foodDiv = $("<div class='food'>");
+
+        // Storing the recipe label
+        var foodlabel = response.hits[0].recipe.label;
+
+        // Source of website URL
+
+        // ingredient list
+        var ingredientList = response.hits[0].recipe.ingredientLines;
+        console.log(ingredientList);
+
+        // create element to hold ingredient
+        var pIngredient = $("<ul>").text("Ingredient list: " + ingredientList)
+
+
+        $("#food-view").text(JSON.stringify(response));
+        console.log(response.hits[0]);
+
+        // render the first 5
+
+    });
+});
+
