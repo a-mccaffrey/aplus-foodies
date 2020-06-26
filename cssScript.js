3d6912a7fe3d221fddcf432c074e81b19df02f62
Mohammed <mohammedyasirsuara@cmail.carleton.ca>
2020-06-25 21:10:56 -0400

    Updated the cssScript and cssIndex files

--- cssScript.js
+++ cssScript.js
@@ -1,60 +1,66 @@
 // jQuery call to allow the dropdown menu function
 $(document).ready(function(){
   $('select').formSelect();
-});
 
-
-/////
-// VARIABLES
-/////
-
-// // This is our API key.
-// var APIKey = "75880f1a2313ab8b5d85907f998f30f7";
-
-
-// $("#find-food").on("click", function(event) {
-
-//     // event.preventDefault() can be used to prevent an event's default behavior.
-//     // Here, it prevents the submit button from trying to submit a form when clicked
-//     event.preventDefault();
-
-//     // Here we grab the text from the input box
-//     var strFood = $("#food-input").val();
-
-//     // Here we construct our URL
-//     var queryURL = "https://api.edamam.com/search?q=" + strFood + "&app_id=ff0891d5" + "&app_key=75880f1a2313ab8b5d85907f998f30f7" + "&from=0&to=3";
-
-//     // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
-//     // and display it in the div with an id of food-view
-    
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function(response){
-
-//         // creating a food div to hold the food
-//         var foodDiv = $("<div class='food'>");
-
-//         // Storing the recipe label
-//         var foodlabel = response.hits[0].recipe.label;
-
-//         // Source of website URL
-
-//         // ingredient list
-//         var ingredientList = response.hits[0].recipe.ingredientLines;
-//         console.log(ingredientList);
-
-//         // create element to hold ingredient
-//         var pIngredient = $("<ul>").text("Ingredient list: " + ingredientList)
-
-
-//         $("#food-view").text(JSON.stringify(response));
-//         console.log(response.hits[0]);
-
-//         // render the first 5
-
-//     });
-// });
-
+  //$('#form').parsley()
+  const apiKey = "6d32852b741eb95d8988031957d11ab4";
+  const apiId = "cd86986f"
   
+  $("#gRep").click(function(){
+    renderSearch()
+  })
 
+  //
+  const renderSearch =()=>{
+    var temp = getURL()
+    let url = "https://api.edamam.com/search?app_id="+apiId+"&app_key="+apiKey+"&"+temp
+    //console.log(url)
+     fetch(url)
+       .then((response)=>{
+         return response.json()
+       })
+       .then((data)=>{
+         showRecipe(data)
+       })
+  }
+
+  //display the recipe
+  const showRecipe = (data)=>{
+    //create a random index
+    var index = Math.floor((Math.random()*10)+1)
+    $("#recipe-title").text(data.hits[index].recipe.label)
+    $(".ingredients-list").empty()
+    var ingNo = data.hits[index].recipe.ingredientLines
+    
+    //append the ingredients to the list
+    for(var i=0; i<ingNo.length; i++){
+      var item = $("<li>")
+      item.text(ingNo[i])
+      $(".ingredients-list").append(item)
+    }
+
+    $("#recipe-instructions").empty()
+    $("#recipe-link").attr('href', data.hits[index].recipe.url)
+    $(".img").attr('src', data.hits[index].recipe.image)
+  }
+
+  //get the url based on the user input
+  const getURL = (e)=>{
+    var input={}
+    var food=$("#food-input").val()
+    var pref=$("#dPreference").val()
+    var label=$("#hLabel").val()
+    if(food){
+      input.q = food
+    }
+    if(pref){
+      input.diet=pref
+    }
+    if(label){
+      input.health=label
+    }
+    console.log($.param(input))
+    return $.param(input)
+  }
+
+});
--- script.js
+++ script.js
@@ -16,11 +16,7 @@ $("#find-food").on("click", function(event) {
     var strFood = $("#food-input").val();
 
     // Here we construct our URL
-<<<<<<< HEAD
-    var queryURL = "https://api.edamam.com/search?q=" + strFood + "&app_id=ff0891d5" + "&app_key=75880f1a2313ab8b5d85907f998f30f7" + "&from=0&to=3";
-=======
     var queryURL = "https://api.edamam.com/search?q=" + strFood + "&app_id=ff0891d5" + "&app_key=75880f1a2313ab8b5d85907f998f30f7";
->>>>>>> master
 
     // Write code between the dashes below to hit the queryURL with $ajax, then take the response data
     // and display it in the div with an id of food-view
@@ -29,36 +25,15 @@ $("#find-food").on("click", function(event) {
         url: queryURL,
         method: "GET"
     }).then(function(response){
-<<<<<<< HEAD
-=======
         console.log(response);
 
         // creating index to hold the 'randomize'
         var indexCreator = Math.floor((Math.random() * 10) + 1);
->>>>>>> master
 
         // creating a food div to hold the food
         var foodDiv = $("<div class='food'>");
 
         // Storing the recipe label
-<<<<<<< HEAD
-        var foodlabel = response.hits[0].recipe.label;
-
-        // Source of website URL
-
-        // ingredient list
-        var ingredientList = response.hits[0].recipe.ingredientLines;
-        console.log(ingredientList);
-
-        // create element to hold ingredient
-        var pIngredient = $("<ul>").text("Ingredient list: " + ingredientList)
-
-
-        $("#food-view").text(JSON.stringify(response));
-        console.log(response.hits[0]);
-
-        // render the first 5
-=======
         var foodlabel = response.hits[indexCreator].recipe.label;
         console.log(foodlabel);
         document.getElementById("food-label").innerHTML += "<b>" + foodlabel + "</b>";
@@ -100,13 +75,10 @@ $("#find-food").on("click", function(event) {
 
         }
 
->>>>>>> master
 
     });
 });
 
-<<<<<<< HEAD
-=======
 
 $("button").click(function(){
     $("div").empty();
@@ -116,4 +88,3 @@ $("button").click(function(){
 
 
 
->>>>>>> master
